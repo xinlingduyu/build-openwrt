db4f2f94c78a00a8c2e48dbb849d9edab8f7eda6
xinlingduyu <52751232+xinlingduyu@users.noreply.github.com>
2023-04-07 12:46:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,10 +10,14 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
+# 开启feeds.conf.default配置表
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
-echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+# 添加插件和主题，取消#注释，代表启用，【电视盒子必须开启第一项（amlogic插件）】
+# echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
+# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+# echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+
+# 替换源码库中软件包，第一行代码先删除源码库中原来的软件，再用第二行代码引入第三方的同名软件包，启用后到 .config 文件里添加第三方软件包。
+# rm -rf package/lean/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
