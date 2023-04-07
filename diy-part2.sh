9bea5a910c80ff997d6c9f1c19b2583ebccabd1e
xinlingduyu <52751232+xinlingduyu@users.noreply.github.com>
2023-04-07 11:02:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
@@ -14,35 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.99/g' package/base-files/files/bin/config_generate
-=======
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.1.99/g' package/base-files/files/bin/config_generate
->>>>>>> 55a0ba4d96ad78f245c320222329ff1729f2e555
-=======
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.1.99/g' package/base-files/files/bin/config_generate
->>>>>>> 55a0ba4d96ad78f245c320222329ff1729f2e555
